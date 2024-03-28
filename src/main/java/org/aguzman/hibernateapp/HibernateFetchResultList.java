f36cfdae7ed3166a5edb5ad732c8c9f8766dcd41
jart <jart2607@gmail.com>
2024-03-27 23:28:54 -0600

    Fetch type Lazy e Eager

---
+++ src/main/java/org/aguzman/hibernateapp/HibernateFetchResultList.java
@@ -0,0 +1,17 @@
+package org.aguzman.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.aguzman.hibernateapp.entity.Cliente;
+import org.aguzman.hibernateapp.util.JpaUtil;
+
+import java.util.List;
+
+public class HibernateFetchResultList {
+    public static void main(String[] args) {
+        EntityManager em= JpaUtil.getEntityManager(); /* no olvidar el fetch que es para poblar y evitar querys */
+        List<Cliente> clientes = em.createQuery("select distinct c from Cliente c left outer join fetch c.direcciones left outer join fetch c.detalle", Cliente.class).getResultList();
+
+        clientes.forEach(c-> System.out.println(c.getNombre()+ ", direcciones: "+c.getDirecciones()));
+        em.close();
+    }
+}
